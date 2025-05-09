d3f92aebd7d62a7fbd1ef4da396674587088f7cc
Nightmare <137179116+NightmareXD0001@users.noreply.github.com>
2025-05-09 16:29:15 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -46,7 +46,7 @@ const Index = () => {
                     className="h-16 mb-4" 
                   />
                 </div>
-                <h2 className="text-2xl md:text-3xl lg:text-5xl font-bold mb-6 text-fear-red minecraft-text">
+                <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold mb-6 text-fear-red minecraft-text">
                   <span className="minecraft-text text-fear-red block mb-4">YOUR NEXT OBSESSION</span>STARTS HERE!
                 </h2>
      
