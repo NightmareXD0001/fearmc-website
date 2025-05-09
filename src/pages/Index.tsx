1868ddbba58cebf2668f98f29e187fa7c22b3cf6
Nightmare <137179116+NightmareXD0001@users.noreply.github.com>
2025-05-09 16:21:18 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -46,9 +46,9 @@ const Index = () => {
                     className="h-16 mb-4" 
                   />
                 </div>
-                <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-6 text-white">
-                  <span className="minecraft-text text-fear-red block mb-2">YOUR NEXT</span> OBSESSION<span className="minecraft-text text-fear-red block mb-2">STARTS HERE !</span>
-                </h1>
+                <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-6 text-white">
+                  <span className="minecraft-text text-fear-red block mb-2">YOUR NEXT</span>OBSESSION<span className="minecraft-text text-fear-red block mb-2">STARTS HERE!</span>
+                </h2>
                 
                 <p className="text-gray-300 mb-8 md:text-lg">
 Welcome to FearMC — a high-stakes, high-reward Minecraft server built for the bold. Whether you're dominating PvP arenas, mastering custom game modes, or just vibing with our ever-growing community, every moment here is unforgettable.
