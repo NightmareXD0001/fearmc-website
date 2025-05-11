5969f11130d7b5de351b4650df5b5394d5af0cbb
Nightmare <137179116+NightmareXD0001@users.noreply.github.com>
2025-05-11 21:45:23 +0530

    Update test-env.ts

--- api/test-env.ts
+++ api/test-env.ts
@@ -1,6 +1,9 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(
+  req: VercelRequest,
+  res: VercelResponse
+) {
   res.status(200).json({
     webhook: process.env.DISCORD_EVENT_WEBHOOK || "Not set",
   });
